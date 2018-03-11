0f9dff41be4a900ccd47706145d02e423e53e9a0
Daedalus's dev <dev.daedalus@gmail.com>
2018-03-11 22:47:58 +0100

    Search in trapp

--- src/CoreComponents/components/Expandable/Expandable.vue
+++ src/CoreComponents/components/Expandable/Expandable.vue
@@ -1,24 +1,24 @@
 <template>
     <v-expansion-panel expand>
-        <v-expansion-panel-content :lazy="true">
-            <div slot="header">{{ value.name }}</div>
+        <v-expansion-panel-content lazy ripple @input="setIsOpen" :value="isOpen">
+            <div slot="header" :style="{opacity: value.matchFilter ? 1 : 0.25}">{{ value.name }}</div>
             <v-card>
                 <v-card-text class="grey lighten-3">
                     <span v-if="elementAttr" v-for="(attr, k) in elementAttr">
                         <strong>{{k}}</strong> : {{attr}}
                     </span>
-                    <expandable
-                        v-if="elementSubStructure"
-                        v-for="childNode in elementSubStructure"
-                        :mapping="mapping"
-                        :value="childNode"
-                        :key="_uid + '-' + childNode.name"
-                    />
+                    <!--<expandable-->
+                        <!--v-if="elementSubStructure"-->
+                        <!--v-for="(childNode, k) in elementSubStructure"-->
+                        <!--:mapping="mapping"-->
+                        <!--v-model="elementSubStructure[k]"-->
+                        <!--:key="_uid + '-' + childNode.name"-->
+                    <!--/>-->
                     <expandable
                         v-if="value.childNodes !== undefined"
-                        v-for="childNode in value.childNodes"
+                        v-for="(childNode, k) in value.childNodes"
                         :mapping="mapping"
-                        :value="childNode"
+                        v-model="value.childNodes[k]"
                         :key="_uid + '-' + childNode.name"
                     />
                 </v-card-text>
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-const EXCLUDE = ['childNodes', 'nodeType', 'name']
+const EXCLUDE = ['childNodes', 'nodeType', 'name', 'isOpen', 'parent', 'childMatch']
 export default {
   name: 'expandable',
   props: {
@@ -37,21 +37,24 @@ export default {
       required: true
     },
     value: {
-      type: Object,
-      required: true
-    },
-    filter: {
       type: Object
     }
   },
-  watch: {
-    filter (filter) {
-      let visible = false
-      if (filter.pattern && this.value.name.search(filter.pattern)) visible = true
-      console.log(visible)
+  methods: {
+    setIsOpen (v) {
+      this.value.isOpen = v
     }
   },
   computed: {
+    isOpen () {
+      return this.value.isOpen || this.value.childMatch
+    },
+    // matchFilter () {
+    //   const {filter} = this
+    //   let matchFilter = true
+    //   if (filter.pattern && this.value.name.search(filter.pattern)) matchFilter = false
+    //   return matchFilter
+    // },
     elementAttr () {
       const {value} = this
       const attr = {}
@@ -62,14 +65,22 @@ export default {
       }
       if (Object.keys(attr).length === 0) return false
       return attr
-    },
-    elementSubStructure () {
-      const {value, mapping} = this
-      if (value.nodeType === 'element' && mapping['xsd'][value.type]) {
-        return mapping['xsd'][value.type]['childNodes'] || false
-      }
-      return false
     }
+    // elementSubStructure () {
+    //   const {value, mapping} = this
+    //   if (value.nodeType === 'element' && mapping['xsd'][value.type]) {
+    //     return mapping['xsd'][value.type]['childNodes'] || false
+    //   }
+    //   return false
+    // }
+    // isOpen: {
+    //   get () {
+    //     return (this.value.isOpen && this.matchFilter) || this.hasOpenChild
+    //   },
+    //   set (v) {
+    //     this.value.isOpen = v
+    //   }
+    // }
   }
 }
 </script>
--- src/router/routes/Trapp.vue
+++ src/router/routes/Trapp.vue
@@ -1,22 +1,138 @@
 <template>
-    <v-container fluid>
-        <v-layout>
-            <expandable
-                    :mapping="mappingTrapp"
-                    :value="mappingTrapp.xsd.PROCEDURE"/>
-        </v-layout>
-    </v-container>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <expandable
+          :mapping="mapping"
+          v-model="mapping.PROCEDURE"
+      />
+
+      <v-slide-x-reverse-transition mode="in-out">
+        <v-container grid-list-md v-show="showFilter" class="filterWrapper card">
+          <v-layout row wrap>
+            <v-flex>
+              <v-form ref="form">
+                Résultats : {{ aResults.length }}
+                  <v-text-field
+                      label="Recherche"
+                      v-model="term"
+                      :loading="searchInProgress"
+                  />
+              </v-form>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-slide-x-reverse-transition>
+      <v-btn
+          color="blue darken-2"
+          dark
+          fab
+          fixed
+          right
+          bottom
+          @click.native="showFilter = !showFilter"
+          v-model="showFilter"
+      >
+        <v-icon>filter_list</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import mappingTrapp from '../../assets/trapp'
+import _ from 'lodash'
+
+const aNodes = []
 export default {
   data () {
+    const mapping = constructionMapping({PROCEDURE: mappingTrapp['xsd']['PROCEDURE']})
     return {
-      mappingTrapp
+      mapping,
+      showFilter: false,
+      term: null,
+      searchInProgress: false
+    }
+  },
+  methods: {
+    applyFilter: _.debounce(function () {
+      const rPattern = this.term ? new RegExp(this.term, 'i') : false
+      aNodes.forEach(function (n) {
+        n.childMatch = false
+        n.matchFilter = true
+        if (rPattern) {
+          const bMatch = rPattern.test(n.name)
+          n.matchFilter = bMatch
+          if (bMatch) {
+            let p = n.parent
+            while (p) {
+              p.childMatch = bMatch
+              p = p.parent
+            }
+          }
+        }
+      })
+      this.searchInProgress = false
+    }, 400)
+  },
+  watch: {
+    term () {
+      this.searchInProgress = true
+      this.applyFilter()
+    }
+  },
+  computed: {
+    aResults () {
+      return aNodes.filter((n) => n.matchFilter)
     }
   }
 }
+
+const mapNode = (o) => {
+  if (_.isObject(o)) {
+    aNodes.push(o)
+    // Init dynamic v-model
+    o.isOpen = false
+    o.childMatch = false
+    o.matchFilter = true
+    // Traitement des nodes
+    if (o.nodeType === 'element' && mappingTrapp['xsd'][o.type]) {
+      o.childNodes = mappingTrapp['xsd'][o.type]['childNodes'] || false
+    } else if (o.nodeType !== 'element' && o.type && o.type.substr(0, 3) !== 'xs:') {
+      o.error = 'Type inconnu'
+    }
+    if (o.childNodes) {
+      constructionMapping(o.childNodes, o)
+    }
+  }
+  return o
+}
+
+const constructionMapping = (o, p) => {
+  if (_.isObject(o)) {
+    _.mapValues(o, (n) => {
+      if (p) {
+        n.parent = p
+        n.path = p.path + '/' + n.name
+      } else {
+        n.path = n.name
+      }
+      return mapNode(n)
+    })
+  }
+  return o
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+  .filterWrapper {
+    position: fixed;
+    bottom: 90px;
+    right: 10px;
+    width: 300px;
+    padding: 24px 16px;
+    max-height: 75vh;
+    overflow: auto;
+    z-index:4;
+  }
+</style>
